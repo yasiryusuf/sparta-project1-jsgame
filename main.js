60ad7c3602266696e305437df9c04ddb1bf22c70
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-11-07 14:13:47 +0000

    the timer is working properly

--- main.js
+++ main.js
@@ -1,27 +1,52 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-$("#start").click( function(){
-  console.log("clicked");
-} )
 
-// start timer
 // setInterval
+$("#start").click( function(){
+  var startTime = 30;
+  var timerrunning = setInterval( function(){ timer() } , 1000);
 
-//score
-// Empty variable set to 0,
-// and every time you click the duck
-// the score variable gets increased by 1
-var score = 0;
-$("#duck").click( function(){
-  //console.log("clicked");
-  score = score + 1;
-  $("#points").html(score);
-} )
+  // Timer Countdown
+  function timer() {
+    startTime = startTime - 1;
+    $("#timer").html(startTime);
+    if (startTime === 0) {
+      //
+      clearInterval(timerrunning);
+    }
+  }
 
+  var score = 0;
+    $("#duck").click( function(){
+    //console.log("clicked");
+    score = score + 1;
+    $("#points").html(score);
+  })
+
+
+
+
+  //
+  // var marginLeftVariable = 0;
+  // $("#duck").animate({
+  //      marginLeft: marginLeftVariable
+  //  }, 2000);
+  //
+  //
+  // for (var i = 1; i <= pageLimit; i++){
+  //     $('#test').append('TESTING');
+  // }
+
+
+})
 
 //ducks movement
 
+// A function to create multiple ducks.
+// A for loop that creates the HTML duck element
+// A random number that determines the direction
+// a function that indefinitely increases the left or right margin of the duck in animate()
 
 
 
