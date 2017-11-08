22352e4dc2b788b65f81d121f888288334aa5eff
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-11-08 09:52:31 +0000

    MVP ready

--- main.js
+++ main.js
@@ -1,21 +1,16 @@
 $(function() {
     console.log( "ready!" );
-
-
-// setInterval
-$("#start").click( function(){
-  var score = 0;
-  var startTime = 30;
-  var timerrunning = setInterval( function(){ timer() } , 1000);
-  var makingDucks = setInterval( function(){ createDuck() } , 1000);
-  
-
+  // setInterval
+  $("#start").click( function(){
+    var score = 0;
+    var startTime = 30;
+    var timerrunning = setInterval( function(){ timer() } , 1000);
+    var makingDucks = setInterval( function(){ createDuck() } , 1000);
   // Timer Countdown
   function timer() {
     startTime = startTime - 1;
     $("#timer").html(startTime);
     if (startTime === 0) {
-      //
       clearInterval(timerrunning);
     }
     if (startTime === 0) {
@@ -26,36 +21,16 @@ $("#start").click( function(){
   function createDuck() {
     var $newduck = $('<div class="ducks"><img id="duck" src="duck.png" height="80px" width="80px" style="position:absolute"></div>');
     $('.middle').append($newduck);
-    //create duck off screen with negative margin
-
+    //score for the game
     $newduck.on('click', function(){
-      console.log("clicked");
-      score = score + 1;
-      $("#points").html(score);
+    console.log("clicked");
+    score = score + 1;
+    $("#points").html(score);
     });
-
-    // Math.random
-
-
+    //duck animating right
     $newduck.animate({
        marginLeft: '100%',
     }, 8000);
   }
-
-
-
-
-
-
-
-
-
-})
-
-
-// game over
-
-
-
-
+  })
 });
