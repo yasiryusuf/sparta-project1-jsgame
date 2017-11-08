a92b7c247a4a93c58c939d377434ac97e06f2d5d
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-11-08 14:00:34 +0000

    done the ugly duckling

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 $(function() {
     console.log( "ready!" );
+
+
+
   // setInterval
   $("#start").click( function(){
     var score = 0;
     var startTime = 30;
     var timerrunning = setInterval( function(){ timer() } , 1000);
     var makingDucks = setInterval( function(){ createDuck() } , 1000);
+    var makingUglyDucks = setInterval( function(){ createUglyDuck() } , 1000);
   // Timer Countdown
   function timer() {
     startTime = startTime - 1;
@@ -29,8 +33,26 @@ $(function() {
     });
     //duck animating right
     $newduck.animate({
-       marginLeft: '100%',
+       marginRight: '120%',
     }, 8000);
   }
+  function createUglyDuck() {
+  var $newduck = $('<div class="duckss"><img id="duck" src="uglyduck.png" height="80px" width="80px" style="position:absolute" style ="marginLeft:180%"> </div>');
+  $('.middle').append($newduck);
+  //create duck off screen with negative margin
+  $newduck.on('click', function(){
+    console.log("clicked");
+    score = score - 2;
+    $("#points").html(score);
+  });
+
+
+  $newduck.animate({
+     marginLeft: '120%',
+  }, 6000);
+}
+
+
   })
+
 });
